0315006c872b1cde7151e86ad09427a6a977c53f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:17:08 +0300

    l

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
-// Create your 'me' object literal here!
+var me={
+	firstName: "Marah",
+    lastName: "ali",
+    age: 16,
+    favoriteFoods: ["fettuccini", "chicken stroganoff"],
+    picture: "https://images.meetmecdna.com/thumb_userimages/2014/01/07/05/thm_tUHBEP6kNL.jpg"
+	}    
+    document.write(JSON.stringify(me))
+
